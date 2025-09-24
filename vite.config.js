0d6bc4bd41f83c3ec8295cb170ac2cef2d6ca033
github.codelaratech.io <165043145+CodeLaraTech@users.noreply.github.com>
2025-09-24 23:48:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/',   // 👈 this part is required for GitHub Pages
+  base: '/Portfolio-web/',   // 👈 this part is required for GitHub Pages
 })
 
